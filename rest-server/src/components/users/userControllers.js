3b2ada15e6b9d10bbe9d204a78c4d1c5159c26ff
Elbert Chao <elbertchao@gmail.com>
2018-03-08 21:23:14 -0800

    add route to get userid by username search

--- rest-server/src/components/users/userControllers.js
+++ rest-server/src/components/users/userControllers.js
@@ -1,6 +1,7 @@
 import db from '../../config/database';
 import {
-  fetchAllUserQuery
+  fetchAllUserQuery,
+  fetchSearchUserQuery
 } from './userQueries';
 import {
   success,
@@ -14,6 +15,17 @@ export const fetchAllUserController = async (req, res) => {
     return res.status(200).send(data);
   } catch (err) {
     error('fetchAllUserController - error= ', error);
+    throw new Error(err);
+  }
+};
+
+export const fetchSearchUserController = async (req, res) => {
+  try {
+    const data = await fetchSearchUserQuery(req.params.username);
+    success('fetchSearchUserController - successfully fetched data ', data);
+    return res.status(200).send(data);
+  } catch (err) {
+    error('fetchSearchUserfetchSearchUserControllerQuery - error= ', error);
     throw new Error(err);
   }
 };
